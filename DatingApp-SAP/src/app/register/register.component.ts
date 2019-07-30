72de7026f833d9653fdb20a81aeffeb7dc0fe6dd
mrnamcpvn <phuongnamcp@gmail.com>
2019-07-30 10:58:19 +0700

    #End of secsion 5

---
+++ DatingApp-SAP/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      this.alertify.success('Registration is successful');
+    }, erorr => {
+      this.alertify.error(erorr);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
